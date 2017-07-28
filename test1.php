97bf99317aebb273a1cae8e575d7495234e0d20d
Ajay <ajay@whiz-solutions.com>
2017-07-28 10:30:28 +0530

    created 2 files says test1.php and test2_master.php

---
+++ test1.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "This hello from test1.php and this file was created in master branch.";
+?>
---
+++ test2_master.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "This hello from test2_master.php and this file was created in master branch.";
+?>
